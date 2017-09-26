a01b4e1a7cf3457b347d8a8d780923dce6a4e89c
Emil Andreas Siemes <emil@Emils-MacBook-Pro.local>
2017-09-26 18:00:45 +0200

    Nicer cicd.sh

--- cicd.sh
+++ cicd.sh
@@ -79,14 +79,17 @@ cd $APP
 echo
 echo We are setting up Jenkins now. 
 read -p "Press button when ready." -n1 -s
+echo
 open $DCOS_URL/service/jenkins/configure
 echo Please add a global environment variable \(under Global properties\) called: DOCKERHUB_REPO and set it to your dockerhub account \(e.g. my is digitalemil\) plus: /$APPLOWERCASE
 echo Then press: Save
 read -p "Press button when ready." -n1 -s
+echo
 echo Now let us connect Jenkins to gitlab which runs in DC/DCOS_URL
 echo First we need to create credentials for gitlab. Please use root as username and rootroot as password, id should read gitlab
 open $DCOS_URL/service/jenkins/credentials/store/system/domain/_/newCredentials
 read -p "Press button when ready." -n1 -s
+echo
 echo We also need to provide Jenkins with your dockerhub username and password. 
 echo id: dockerhub 
 open $DCOS_URL/service/jenkins/credentials/store/system/domain/_/newCredentials
@@ -95,8 +98,10 @@ echo
 echo Next step is to create the build pipleine. In the browser window please call the item $APP and select Pipeline as type and then press OK
 open $DCOS_URL/service/jenkins/view/all/newJob
 read -p "Press button when ready." -n1 -s
-echo Now check Poll SCM and use \"* * * * *\" as schedule. Press Apply. Scroll down to Pipeline and select \"Pipeline script from SCM\". Select Git as SCM
+echo
+echo Now check Poll SCM and use "* * * * *" as schedule. Press Apply. Scroll down to Pipeline and select \"Pipeline script from SCM\". Select Git as SCM
 read -p "Press button when ready." -n1 -s
+echo
 echo Next we need to define the repository. Please enter http://gitlab.marathon.l4lb.thisdcos.directory/root/$APP.git as Repository URL and select root/******** as credentials. Press Apply
 echo We are all set now. Thank you for your patience. You can now start build pipelines in Jenkins or call the upgrade.sh or downgrade.sh script in the folder where we cloned the repo.
 echo Good luck!
